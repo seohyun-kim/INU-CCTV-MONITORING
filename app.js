e8f52dc4e9425e3ae1b35d9691920d30a90053ab
seohyun-kim <selenium34@inu.ac.kr>
2021-08-18 23:24:21 +0900

    db info template

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-//frame
+const mysql = require('mysql');
+const express = require('express');
+const ejs = require('ejs');
+const schedule = require('node-schedule');
+const cookie = require('cookie-parser');
+const session = require('express-session');
+const http=require('http');
+const socketio = require('socket.io');
+const MySQLEvents = require('mysql-events');
+
+var conn = require('./conf/db'); // db접속정보
