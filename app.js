7459202bd7ab293380df27787e94ba133a839418
seokahi <skh9797@naver.com>
2021-08-22 15:45:54 +0900

    add comment and a little modification

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const http=require('http');
 const socketio = require('socket.io');
 const MySQLEvents = require('mysql-events');
 const cors = require('cors');
-var conn = require('./conf/db');//mysql 연결
+var conn = require('./conf/db_template');//mysql 연결
 
 //express 사용
 const app = express();
@@ -63,7 +63,7 @@ io.on("connection", (socket) => {
                     console.log(err);
                 } else {
                     console.log("보낸다");
-                    socket.emit("datainsert", row[0]);
+                    socket.emit("outlierData", row[0]);
                   //  console.log(row[0]);
                 }
             });
@@ -126,69 +126,12 @@ app.get('/mainPage', (req, res) => {
     });
 });
 
-// 데이터 새로고침
-app.post('/mainPage/refreshData', (req, res) => {
-    console.log(req.body);
-    var sql = 'select * from test order by id desc limit 1'
-    conn.query(sql, (err, row) => {
-        if (err) {
-            console.log(err);
-        } else {
-            //console.log(row);
-            res.json(row[0]); // latest row
-        }
-    });
-});
 
-
-//합계 데이터 조회 - 기간 조회
-app.post('/mainPage/dateSelect', (req, res) => {
-    var data = req.body;
-    var sql = 'select sum(people), sum(vehicle) from test where start_date >= ? and end_date <= ?'
-    conn.query(sql, [data.startDate, data.endDate], (err, row) => {
-        if (err) {
-            console.log(err);
-        } else {
-            //console.log(row);
-            res.json(row[0]); // sum(people), sum(vehicle)
-        }
-    });
-});
-
-//합계 데이터 조회 - 일간 조회
-app.post('/mainPage/daySelect', (req, res) => {
-    var data = req.body;
-    var sql = "select sum(people), sum(vehicle) from test WHERE start_date >= CONCAT(?,' 00:00:00') AND start_date <= CONCAT(?,' 23:59:59')";
-    conn.query(sql,[data._Date,data._Date], (err, row) => {
-        if (err) {
-            console.log(err);
-        } else {
-            //console.log(row);
-            res.json(row[0]); // sum(people), sum(vehicle)
-        }
-    });
-});
-
-//합계 데이터 조회 - 시간 조회
-app.post('/mainPage/timeSelect', (req, res) => {
-    var data = req.body;
-    var sql =  "select sum(people), sum(vehicle) from test WHERE start_date >= CONCAT(?,' ',?,':00') AND start_date <= CONCAT(?,' ',?,':00')";
-    conn.query(sql, [data.start_Date,data.startTime,data.end_Date,data.endTime], (err,row) => {
-        if (err) {
-            console.log(err);
-        } else {
-            //console.log(row);
-            res.json(row[0]); // sum(people), sum(vehicle)
-        }
-    });
-});
-
-
-//시작시간/종료시간을 지정하고, 수신한 데이터를 테이블로 표시
+//시작시간/종료시간을 지정하고, 수신한 데이터를 그래프로 표시
 app.post('/mainPage/tableDatetimeSelect', (req, res) => {
     var data = req.body;
     console.log(data.startDate);
-    var sql = "SELECT * FROM cctv_data WHERE date_time >= CONCAT(?,' 00:00:00') AND date_time <= CONCAT(?,' 23:59:59')";
+    var sql = "SELECT * FROM cctv_data WHERE date_time >=? AND date_time <= ?";
     conn.query(sql, [data.startDate, data.endDate], (err, row) => {
         if (err) {
             console.log(err);
@@ -198,10 +141,11 @@ app.post('/mainPage/tableDatetimeSelect', (req, res) => {
         }
     });
 });
+//시작시간/종료시간에 따른 차량들의 합계
 app.post('/mainPage/tableDatetimeSelect2', (req, res) => {
     var data = req.body;
     console.log(data.startDate);
-    var sql = "SELECT sum(n_car), sum(n_bicycle), sum(n_motorcycle),sum(n_truck) FROM cctv_data WHERE date_time >= CONCAT(?,' 00:00:00') AND date_time <= CONCAT(?,' 23:59:59')";
+    var sql = "SELECT sum(n_car), sum(n_bicycle), sum(n_motorcycle),sum(n_truck) FROM cctv_data WHERE date_time >=? AND date_time <= ?";
     conn.query(sql, [data.startDate, data.endDate], (err, row) => {
         if (err) {
             console.log(err);
@@ -211,10 +155,11 @@ app.post('/mainPage/tableDatetimeSelect2', (req, res) => {
         }
     });
 });
+//시작시간/종료시간에 따른 사람들의 수
 app.post('/mainPage/showPerson', (req, res) => {
     var data = req.body;
     console.log(data._Date);
-    var sql = "SELECT sum(n_person) FROM cctv_data WHERE  date_time >= CONCAT(?,' 00:00:00') AND date_time <= CONCAT(?,' 23:59:59')";
+    var sql = "SELECT sum(n_person) FROM cctv_data WHERE  date_time >= ? AND date_time <= ?";
     conn.query(sql,[data.startDate, data.endDate], (err, row) => {
         if (err) {
             console.log(err);
@@ -225,6 +170,7 @@ app.post('/mainPage/showPerson', (req, res) => {
     });
 
 });
+//시간을 지정하고, 수신한 데이터를 그래프로 표시
 app.post('/mainPage/tableDaytimeSelect', (req, res) => {
     var data = req.body;
     console.log(data._Date);
@@ -239,6 +185,7 @@ app.post('/mainPage/tableDaytimeSelect', (req, res) => {
     });
 
 });
+//시간에 따른 차량들의 합계
 app.post('/mainPage/tableDaytimeSelect2', (req, res) => {
     var data = req.body;
     console.log(data._Date);
@@ -253,6 +200,7 @@ app.post('/mainPage/tableDaytimeSelect2', (req, res) => {
     });
 
 });
+//시간에 따른 사람들의 수
 app.post('/mainPage/showpperson', (req, res) => {
     var data = req.body;
     console.log(data._Date);
@@ -267,38 +215,40 @@ app.post('/mainPage/showpperson', (req, res) => {
     });
 
 });
-app.post('/mainPage/carSpeed', (req, res) => {
-    var data = req.body;
-    console.log(data._Date);
-    var sql = "SELECT sum(n_car), sum(n_bicycle), sum(n_motorcycle),sum(n_truck) FROM cctv_data WHERE  date_time >= CONCAT(?,' 00:00:00') AND date_time <= CONCAT(?,' 23:59:59')";
-    conn.query(sql, [data._Date, data._Date], (err, row) => {
-        if (err) {
-            console.log(err);
-        } else {
-            //console.log(row);
-            res.json(row);
-        }
-    });
+//시간에 따른 차량속도 --> 나중에
+// app.post('/mainPage/carSpeed', (req, res) => {
+//     var data = req.body;
+//     console.log(data._Date);
+//     var sql = "SELECT sum(n_car), sum(n_bicycle), sum(n_motorcycle),sum(n_truck) FROM cctv_data WHERE  date_time >= CONCAT(?,' 00:00:00') AND date_time <= CONCAT(?,' 23:59:59')";
+//     conn.query(sql, [data._Date, data._Date], (err, row) => {
+//         if (err) {
+//             console.log(err);
+//         } else {
+//             //console.log(row);
+//             res.json(row);
+//         }
+//     });
 
-});
-app.post('/mainPage/showimg', (req, res) => {
-    var data = req.body;
-    console.log(data._Date);
+// });
+//이미지 보여주는 것 --> 나중에
+// app.post('/mainPage/showimg', (req, res) => {
+//     var data = req.body;
+//     console.log(data._Date);
 
-    var sql = "select image from cctv_data order by date_time desc limit 1";
-    conn.query(sql, (err, row) => {
-        if (err) {
-            console.log(err);
-        } else {
-            for (i = 0; i <= row.length - 1; i++) {
-                row[i].image = Buffer.from(row[i].image).toString('base64');
-            }
+//     var sql = "select image from cctv_data order by date_time desc limit 1";
+//     conn.query(sql, (err, row) => {
+//         if (err) {
+//             console.log(err);
+//         } else {
+//             for (i = 0; i <= row.length - 1; i++) {
+//                 row[i].image = Buffer.from(row[i].image).toString('base64');
+//             }
 
-            res.render('./part/chart.ejs', { data: row });
-        }
-    });
+//             res.render('./part/chart.ejs', { data: row });
+//         }
+//     });
 
-});
+// });
 
 
 //메인페이지 로그아웃
@@ -311,37 +261,37 @@ app.post('/mainPage/logout', (req, res) => {
 
 
 // Create Sample Data
-app.get('/createSampleData', (req, res) => {
-    res.render('./createDataPage.ejs');
-});
+// app.get('/createSampleData', (req, res) => {
+//     res.render('./createDataPage.ejs');
+// });
 
-app.post('/createSampleData', (req, res) => {
-    console.log(req.body);
-    var data = req.body;
+// app.post('/createSampleData', (req, res) => {
+//     console.log(req.body);
+//     var data = req.body;
 
-    var startTime = new Date(data.startDate).setHours(data.startTime);
-    var endTime = new Date(data.endDate).setHours(data.endDate);
+//     var startTime = new Date(data.startDate).setHours(data.startTime);
+//     var endTime = new Date(data.endDate).setHours(data.endDate);
 
-    var flag = 1;
-    var job = schedule.scheduleJob({
-        start: startTime,
-        end: endTime+1,
-        rule: '*/5 * * * *' // 주기 (5분마다)
-    },
-    function() {
-        if (flag == 1) {
-            res.json('Scheduler is Working');
-            flag = flag + 1;
-        }
+//     var flag = 1;
+//     var job = schedule.scheduleJob({
+//         start: startTime,
+//         end: endTime+1,
+//         rule: '*/5 * * * *' // 주기 (5분마다)
+//     },
+//     function() {
+//         if (flag == 1) {
+//             res.json('Scheduler is Working');
+//             flag = flag + 1;
+//         }
 
-        var data = createData(5*60);
-        var sql = 'INSERT into test(start_date, end_date, people, vehicle) value (?, ?, ?, ?)';
-        conn.query(sql, [data.startDate, data.endDate, data.peopleNum, data.vehicleNum], (err) => {
-            if (err) {
-                console.log(err);
-            } else {
-                console.log('complete insert data');
-            }
-        });
-    });
-})
+//         var data = createData(5*60);
+//         var sql = 'INSERT into test(start_date, end_date, people, vehicle) value (?, ?, ?, ?)';
+//         conn.query(sql, [data.startDate, data.endDate, data.peopleNum, data.vehicleNum], (err) => {
+//             if (err) {
+//                 console.log(err);
+//             } else {
+//                 console.log('complete insert data');
+//             }
+//         });
+//     });
+// })
